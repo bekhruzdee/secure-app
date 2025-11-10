10b1a04d609292f91127e727f385d17a7aef7285
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-11-10 17:08:59 +0500

    update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,25 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { UsersService } from './users.service';
-// import { UsersController } from './users.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from './entities/user.entity';
-// import { JwtModule } from '@nestjs/jwt';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forFeature([User]),
-//     JwtModule.register({
-//       global: true,
-//       secret: process.env.JWT_SECRET,
-//       signOptions: { expiresIn: process.env.JWT_EXPIRATION },
-//     }),
-//   ],
-//   controllers: [UsersController],
-//   providers: [UsersService],
-//   exports: [UsersService],
-// })
-// export class UsersModule {}
-
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
